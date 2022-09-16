c002194accf96cd6508e156cf9bc9175f39a0a56
ic__e <yoshidashi058@gmail.com>
2022-09-17 02:15:45 +0900

    effectの変更

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,20 +1,13 @@
 import React, {useEffect, useState} from 'react'
 
 const Test = ({text}): JSX.Element => {
-  const [content, setContent] = useState('love cat! love dog!')
-  const [textCur, setTextCur] = React.useState("")
+  const [textCur, setTextCur] = useState("")
+  const [display, setDisplay] = useState(false)
 
   const coverArrayBoolean = (coverArray: Array<boolean>): void => {
-    if (coverArray.length > 128) {
-      // fill with true
-      for (let i = 0; i < coverArray.length; i++) {
-        coverArray[i] = true
-      }
-      return
-    }
 
     // if not covered, cover it
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 2; i++) {
       // gen random index of 0 to text.length
       const randomNum = Math.floor(Math.random() * text.length)
       if (coverArray[randomNum] === false) {
@@ -24,54 +17,39 @@ const Test = ({text}): JSX.Element => {
     }
   }
 
-  const IsCharJapaneseOrEmoji = (char: string): boolean => {
-    const charCode = char.charCodeAt(0)
-    return (charCode >= 0x3040 && charCode <= 0x30FF) || (charCode >= 0x31F0 && charCode <= 0x31FF)
-  }
-
   const isFilled = (covered: Array<boolean>): boolean => {
     return !covered.includes(false)
   }
 
-const addCat= () => {
-  let cat = content
-  let element = ''
-  for (let index = 0; index < cat.length; index++) {
-    element += '*'
-  }
-  cat += 'cat'
-  setContent(element)
-}
+  useEffect(() =>  {
+    let changed = Array(text.length).fill(false)
+    const sym = [",", ".", "/", "^", "¥", "*", "*", "_", "#", "$", "!", "&"]
+    const love = setInterval(() => {
 
-useEffect(() =>  {
-  let covered = Array(text.length).fill(false)
-  console.log(covered)
-  const id = setInterval(() => {
-
-    if (isFilled(covered)) {
-      clearInterval(id)
-      return
-    }
-
-    for (let i = 0; i < 3; i++) {
-      coverArrayBoolean(covered)
-    }
-
-    // gen random text
-    const newText = text.split("").map((char, index) => {
-      if (covered[index]) {
-        return char
-      } else {
-        if (char == " ") {
-          return " "
-        }
-        return IsCharJapaneseOrEmoji(char) ? "＊" : "-"
+      if (isFilled(changed)) {
+        clearInterval(love)
+        return
       }
-    })
-    setTextCur(newText.join(""))
-  }, 1000/60)
-  return () => clearInterval(id)
-}, [])
+
+      for (let i = 0; i < 2; i++) {
+        coverArrayBoolean(changed)
+      }
+
+      // gen random text
+      const newText = text.split("").map((char, index) => {
+        if (changed[index]) {
+          return char
+        } else {
+          if (char === " ") {
+            return " "
+          }
+          return sym[Math.floor(Math.random() * sym.length)]
+        }
+      })
+      setTextCur(newText.join(""))
+    }, 30)
+    return () => clearInterval(love)
+  }, [])
 
 
   return (
