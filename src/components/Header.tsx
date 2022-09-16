1e643093e3927f68fe0d8b238f94e54168b2b783
ic__e <yoshidashi058@gmail.com>
2022-09-17 02:13:53 +0900

    logoにtypewriter-effect適用

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Menu from './Menu'
+import Typewriter from 'typewriter-effect'
 
 const Header = () => {
   return (
     <header>
-      <div className="logo">ic_e</div>
+      <div className="logo">
+        <Typewriter
+          options = {{
+            autoStart: true,
+            loop: true
+          }}
+          onInit = {(typewriter) => {
+            typewriter.typeString("#####")
+            .pauseFor(100)
+            .typeString('.')
+            .pauseFor(100)
+            .typeString('.')
+            .pauseFor(100)
+            .typeString('.')
+            .pauseFor(10000)
+            .start()
+          }}
+        /> 
+      </div>
       <nav>
         <Menu />
       </nav>
