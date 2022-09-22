0342bd81e2d64e3d61a4296751f5aa6f80a2c738
ic__e <yoshidashi058@gmail.com>
2022-09-22 23:36:10 +0900

    contents 修正

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import Test from './Test'
+import Modelviewer from './Modelviewer'
 
 const Home = () => {
   return (
     <div className="content-wrapper">
       <div className='content'>
-        <Test text={"#name -> syuto yoshida"}/>
-        <Test text={"#age  -> 22"}/>
-        <Test text={"#skill -> react, node.js, TypeScript, Next.js"}/>
-        <Test text={"#my best movie -> clock works of orage"}/>
-        <Test text={"#love -> music movie"}/>
+        <Test text={"#################################################################"}/>
+        <Test text={"-"}/>
+        <Test text={"name -> syuto yoshida"}/>
+        <Test text={"age  -> 22"}/>
+        <Test text={"skill -> react, node.js, TypeScript, Next.js"}/>
+        <Test text={"my best movie -> clock works of orage"}/>
+        <Test text={"love -> music movie"}/>
         <Test text={'String name = "syuto yoshida"'}/>
         <Test text={"int age = 22"}/>
         <Test text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
-        <Test text={"#my best movie -> clock works of orage"}/>
-        <Test text={"#love -> music movie"}/>
+        <Test text={"my best movie -> clock works of orage"}/>
+        <Test text={"love -> music movie"}/>
+        <Test text={"-"}/>
+        <Test text={"#################################################################"}/>
       </div>
     </div>
   )
