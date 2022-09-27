fa987fc69756d42a55fc8df59eeb70e067c55526
ic__e <yoshidashi058@gmail.com>
2022-09-27 23:54:57 +0900

    ブログページ

---
+++ src/components/Blog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shuffle from './Shuffle'
+
+const Blog = () => {
+  return (
+    <div className="content-wrapper">
+      <div className="content">
+        <Shuffle text={"lovelovelovelovelvoelovelovelovelove"}/>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
