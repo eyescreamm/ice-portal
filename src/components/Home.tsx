2d1661d8cd175b5efba114d5fd71dac372ead15d
ic__e <yoshidashi058@gmail.com>
2022-09-29 13:11:37 +0900

    文言変更

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,52 @@
-import React from 'react'
-import Test from './Test'
-import Modelviewer from './Modelviewer'
+import React from "react"
+import Shuffle from "./Shuffle"
 
 const Home = () => {
   return (
     <div className="content-wrapper">
-      <div className='content'>
-        <Test text={"#################################################################"}/>
-        <Test text={"-"}/>
-        <Test text={"name -> syuto yoshida"}/>
-        <Test text={"age  -> 22"}/>
-        <Test text={"skill -> react, node.js, TypeScript, Next.js"}/>
-        <Test text={"my best movie -> clock works of orage"}/>
-        <Test text={"love -> music movie"}/>
-        <Test text={'String name = "syuto yoshida"'}/>
-        <Test text={"int age = 22"}/>
-        <Test text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
-        <Test text={"my best movie -> clock works of orage"}/>
-        <Test text={"love -> music movie"}/>
-        <Test text={"-"}/>
-        <Test text={"#################################################################"}/>
+      <div className="content">
+        <Shuffle text={"#"}/>
+        <Shuffle text={"name -> syuto yoshida"}/>
+        <Shuffle text={"age  -> 22"}/>
+        <Shuffle text={"skill -> react, node.js, TypeScript, Next.js"}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={'String name = "syuto yoshida"'}/>
+        <Shuffle text={"int age = 22"}/>
+        <Shuffle text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={"#"}/>
+        <Shuffle text={"name -> syuto yoshida"}/>
+        <Shuffle text={"age  -> 22"}/>
+        <Shuffle text={"skill -> react, node.js, TypeScript, Next.js"}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={'String name = "syuto yoshida"'}/>
+        <Shuffle text={"int age = 22"}/>
+        <Shuffle text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={"name -> syuto yoshida"}/>
+        <Shuffle text={"age  -> 22"}/>
+        <Shuffle text={"skill -> react, node.js, TypeScript, Next.js"}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={'String name = "syuto yoshida"'}/>
+        <Shuffle text={"int age = 22"}/>
+        <Shuffle text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={"name -> syuto yoshida"}/>
+        <Shuffle text={"age  -> 22"}/>
+        <Shuffle text={"skill -> react, node.js, TypeScript, Next.js"}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
+        <Shuffle text={'String name = "syuto yoshida"'}/>
+        <Shuffle text={"int age = 22"}/>
+        <Shuffle text={'String[] language = ["react", "node.js", "TypeScript", "Next.js"]'}/>
+        <Shuffle text={"my best movie -> clock works of orage"}/>
+        <Shuffle text={"love -> music movie"}/>
       </div>
     </div>
   )
