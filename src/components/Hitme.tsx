fcf5838101e257b16f1198f3e3067b0d4537c238
ic__e <yoshidashi058@gmail.com>
2022-10-04 07:37:11 +0900

    Create Hitme.tsx

---
+++ src/components/Hitme.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shuffle from './Shuffle'
+
+const Hitme = () => {
+  return (
+    <div className="content-wrapper">
+      <div className="content">
+        <Shuffle text={"Hit Me! Hit Me! Hit Me! Hit Me! Hit Me!"}/>
+      </div>
+    </div>
+  )
+}
+
+export default Hitme
