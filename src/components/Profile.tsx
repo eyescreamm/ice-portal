04da7d521da40286e50d1f20cb71e82f8529224b
ic__e <yoshidashi058@gmail.com>
2022-10-04 07:37:17 +0900

    Create Profile.tsx

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Shuffle from './Shuffle'
+
+const Profile = () => {
+  return (
+    <div className="content-wrapper">
+      <div className="content">
+        <Shuffle text={"ProfileProfileProfileProfileProfileProfile"}/>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
