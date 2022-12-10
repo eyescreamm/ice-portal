15c78b04e1c28e51656ce793d1c831d60b95013c
ic__e <yoshidashi058@gmail.com>
2022-12-10 22:20:35 +0900

    docs: update product

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,9 +6,9 @@ const Products = () => {
   return (
     <div className="content-wrapper">
       <div className="content">
-        <Link to="https://ice-blog.vercel.app/">
-          <Shuffle text={'# Personal blog'} />
-        </Link>
+        <a target="_blank" href="https://ice-blog.vercel.app/" rel="noreferrer">
+          <Shuffle className={'menu-item'} text={'# Personal blog'} />
+        </a>
         <Shuffle text={'(TypeScript, React, Nextjs, GraphQL)'} />
       </div>
     </div>
