3fb53a90ab8c7185424f8a42812b8c51fdd18f95
ic__e <yoshidashi058@gmail.com>
2022-12-30 10:26:49 +0900

    docs: update products docs

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,10 +6,24 @@ const Products = () => {
   return (
     <div className="content-wrapper">
       <div className="content">
+        <Shuffle className={'head css-selector'} text={'products'} />
+        <Shuffle text={'-------------'} />
         <a target="_blank" href="https://ice-blog.vercel.app/" rel="noreferrer">
           <Shuffle className={'menu-item'} text={'# Personal blog'} />
         </a>
-        <Shuffle text={'(TypeScript, React, Nextjs, GraphQL)'} />
+        <Shuffle newLine={false} text={'github code ->'} />
+        <span>&nbsp;</span>
+        <a
+          target="_blank"
+          href="https://github.com/eyescreamm/cms-blog"
+          rel="noreferrer"
+        >
+          <Shuffle
+            newLine={false}
+            text={'https://github.com/eyescreamm/cms-blog'}
+          />
+        </a>
+        <Shuffle text={'about -> TypeScript, React, Nextjs, GraphQL'} />
       </div>
     </div>
   );
